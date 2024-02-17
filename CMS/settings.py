110d204dd0763c19f59cd3c0647dade24bc4bc1d
VGD3626 <vrund3626@gmail.com>
2024-02-17 21:10:40 +0530

    2nd commit on 17-02-24

--- CMS/settings.py
+++ CMS/settings.py
@@ -127,6 +127,6 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_URL = '/static/'
+MEDIA_URL = '/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
